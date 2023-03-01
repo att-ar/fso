d72b8410779265edcc402ea6cf601bb7000295c7
Attar Aziz <attar_a@outlook.com>
2023-03-01 16:06:21 -0500

    Part 1 unicafe complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,65 +7,67 @@ const Button = ({ handleClick, text }) => {
     return <button onClick={handleClick}>{text}</button>;
 };
 
-const Display = ({ state, text }) => {
+const StatisticLine = ({ text, value, suffix = "" }) => {
     //gets the state and its corresponding text
+    //adding style
+    // <td style={{ border: "solid", backgroundColor: "lightblue" }}>{text} </td>;
     return (
-        <>
-            {text} {state}
-            <br></br>
-        </>
-    );
-};
-const Total = ({ states }) => {
-    // gets a list of objects containing all the states
-    const init_val = 0;
-    const all = states.reduce(
-        (prev_count, count) => prev_count + count.state[0],
-        init_val
-    );
-    return (
-        <>
-            all {all}
-            <br></br>
-        </>
-    );
-};
-const Mean = ({ states }) => {
-    // states is a list of objects containing
-    // a list with the state and its score value
-    // [{_: [_, _]}, ...]
-    const init_val = [0, 0];
-    const score = states.reduce(
-        (prev_tuple, count) => [
-            prev_tuple[0] + count.state[0],
-            prev_tuple[1] + count.state[0] * count.state[1],
-        ],
-        init_val
-    );
-    return (
-        <>
-            average {score[1] / score[0]}
-            <br></br>
-        </>
+        <tr>
+            <td>{text}</td>
+            <td>
+                {value} {suffix}
+            </td>
+        </tr>
     );
 };
 
-const Positive = ({ states }) => {
-    const positive_count = states[0].state[0];
-    const count = states.reduce(
-        (prev_tuple, count) => prev_tuple + count.state[0],
+const Statistics = ({ states }) => {
+    const total = states.reduce(
+        (prev_state, cur_state) => prev_state + cur_state.state[0],
         0
     );
-    console.log("Count:", count);
-    console.log("Positive:", positive_count);
-    return (
-        <>
-            positive {positive_count / count}
-            <br></br>
-        </>
-    );
-};
 
+    console.log("total inside Statistics:", total);
+    if (total > 0) {
+        const good_tuple = states[0].state;
+        const neutral_tuple = states[1].state;
+        const bad_tuple = states[2].state;
+
+        const mean_score =
+            (good_tuple[0] * good_tuple[1] +
+                neutral_tuple[0] * neutral_tuple[1] +
+                bad_tuple[0] * bad_tuple[1]) /
+            total;
+        const positive_percent = (100 * good_tuple[0]) / total;
+
+        console.log("mean score", mean_score);
+        console.log("positive percent", positive_percent);
+
+        return (
+            <>
+                <table>
+                    <tbody>
+                        <StatisticLine text="good" value={good_tuple[0]} />
+                        <StatisticLine
+                            text="neutral"
+                            value={neutral_tuple[0]}
+                        />
+                        <StatisticLine text="bad" value={bad_tuple[0]} />
+                        <StatisticLine text="all" value={total} />
+                        <StatisticLine text="average" value={mean_score} />
+                        <StatisticLine
+                            text="positive"
+                            value={positive_percent}
+                            suffix="%"
+                        />
+                    </tbody>
+                </table>
+            </>
+        );
+    } else {
+        return <p>No feedback given</p>;
+    }
+};
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0);
@@ -86,22 +88,23 @@ const App = () => {
 
     return (
         <>
-            <Header text="give feedback" />
-            <Button handleClick={handleClickFn(good, setGood)} text="good" />
-            <Button
-                handleClick={handleClickFn(neutral, setNeutral)}
-                text="neutral"
-            />
-            <Button handleClick={handleClickFn(bad, setBad)} text="bad" />
-            <Header text="statistics" />
+            <div>
+                <Header text="give feedback" />
+                <Button
+                    handleClick={handleClickFn(good, setGood)}
+                    text="good"
+                />
+                <Button
+                    handleClick={handleClickFn(neutral, setNeutral)}
+                    text="neutral"
+                />
+                <Button handleClick={handleClickFn(bad, setBad)} text="bad" />
+            </div>
 
-            <Display state={good} text="good" />
-            <Display state={neutral} text="neutral" />
-            <Display state={bad} text="bad" />
-
-            <Total states={states} />
-            <Mean states={states} />
-            <Positive states={states} />
+            <div>
+                <Header text="statistics" />
+                <Statistics states={states} />
+            </div>
         </>
     );
 };
