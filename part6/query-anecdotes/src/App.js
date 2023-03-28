de827160e77109f4e44ca06f81a2c7be7138098e
Attar Aziz <attar_a@outlook.com>
2023-03-27 22:02:40 -0400

    Part 6 ex 6.20 complete

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,40 +1,61 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import { useQuery } from "react-query";
+
+import { getAnecdotes } from "./requests";
+
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
 
 const App = () => {
+    const handleVote = (anecdote) => {
+        console.log("vote");
+    };
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+    // const anecdotes = [
+    //   {
+    //     "content": "If it hurts, do it more often",
+    //     "id": "47145",
+    //     "votes": 0
+    //   },
+    // ]
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+    const result = useQuery("anecdote", getAnecdotes, {
+        refetchOnWindowFocus: false,
+        retry: 1,
+    });
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
+    console.log(result);
+
+    if (result.isLoading) {
+        return <div>loading data...</div>;
+    }
+    if (result.isError) {
+        return (
+            <div>anecdote service not available due to problems in server</div>
+        );
+    }
+
+    const anecdotes = result.data;
+
+    return (
+        <div>
+            <h3>Anecdote app</h3>
+
+            <Notification />
+            <AnecdoteForm />
+
+            {anecdotes.map((anecdote) => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => handleVote(anecdote)}>
+                            vote
+                        </button>
+                    </div>
+                </div>
+            ))}
         </div>
-      )}
-    </div>
-  )
-}
+    );
+};
 
-export default App
+export default App;
---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/anecdotes";
+
+export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
+
+export const createAnecdote = (newAnecdote) =>
+    axios
+        .post(`${baseUrl}/${newAnecdote.id}`, newAnecdote)
+        .then((res) => res.data);
+
+export const updatedAnecdote = (updatedAnecdote) =>
+    axios
+        .put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
+        .then((res) => res.data);
